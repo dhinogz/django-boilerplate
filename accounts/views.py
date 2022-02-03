9a65ec911247688303b47b214983cd8a49c1d8f2
dhinogz <davidhinojosauni@gmail.com>
2022-02-03 03:20:20 -0600

    Clean up

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,1 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import generic
-from .forms import CustomUserCreationForm
-
-"""
-class SignupPageView(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-"""
+# All-auth accomplishes our need for views in the accounts app!
--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT = 'home'
 SITE_ID = 1
-AUTHENTICATION_BACKEND = (
+AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
